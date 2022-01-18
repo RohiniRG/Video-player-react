16941b7bbd70ea05bf4b95582e20e8edb40933f2
Rohini Rao <rohinirao2112@gmail.com>
2022-01-18 14:03:34 +0530

    Created started project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react"
+import ReactDOM from "react-dom" 
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//  Create a new REACT component. This component produces some HTML
+const App =  () => {
+    // HTML in between is known as JSX, which looks like HTML but is JS itself
+    // This code is later passed through webpack and babel to transpile and create vanilla JS  like code
+    // which ultimately produces HTML 
+    return <div>Heyloo!</div>
+}
+  
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container')) 
